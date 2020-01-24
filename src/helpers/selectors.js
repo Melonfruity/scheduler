05809e16ddec1a38a73a4c6ceee1bfa4914035d0
melonfruity <eric.luo24@gmail.com>
2020-01-23 19:17:32 -0500

    done and tested getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,15 @@
 
+export const getInterview = (state, interview) => {
+
+  if (!interview) return null;
+
+  const formattedInterview = {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer],
+  }
+
+  return formattedInterview;
+}
 
 export const getInterviewersForDay = (state, day) => {
   const dayOf = state.days.filter((d) => d.name === day)[0];

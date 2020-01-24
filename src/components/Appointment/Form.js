31c490e78f6e4a09f3994a299430e10be229d0c1
melonfruity <eric.luo24@gmail.com>
2020-01-23 19:20:30 -0500

    added Interviewerlist to Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,10 @@ const Form = (props) => {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer ? props.interviewer.id : null)
 
+  const reset = () => {
+    setName("");
+    setInterviewer("");
+  }
 
   const cancel = () => {
     reset();
@@ -29,11 +33,7 @@ const Form = (props) => {
             data-testid="student-name-input"
           />
         </form>
-        <InterviewerList
-          interviewers={props.interviewers} 
-          interviewer={interviewer} 
-          setInterviewer={setInterviewer}
-        />
+        
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">

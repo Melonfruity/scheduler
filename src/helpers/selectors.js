bc1421e288c7bf6dd52b6ff0691776985d3f5180
melonfruity <eric.luo24@gmail.com>
2020-01-23 19:17:43 -0500

    done getAppointmentForDays

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,21 @@
+export const getAppointmentsForDay = (state, day) => {
+  const { days, appointments } = state;
+  const selectedDay = days.find((d) => d.name === day);
+  const selectedAppointments = [];
+  
+  if (!selectedDay) {
+    return selectedAppointments;
+  }
+
+  for (let index in appointments) {
+    const id = appointments[index].id;
+    if (selectedDay.appointments.includes(id)) {
+      selectedAppointments.push(appointments[index]);
+    }
+  }
+  
+  return selectedAppointments;
+}
 
 export const getInterview = (state, interview) => {
 

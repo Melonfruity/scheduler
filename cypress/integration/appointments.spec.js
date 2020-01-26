e68771d8cda1a29ed1e06ea6c7d16d02b4d89ba9
melonfruity <eric.luo24@gmail.com>
2020-01-25 23:24:55 -0500

    should book an interview cypress test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,19 +6,19 @@ describe("Appointments", () => {
     
   })
   
-  // it("should book an interview", () => {
-  //   cy.get("[alt='Add']")
-  //     .first()
-  //     .click()
-  //     .get("[data-testid=student-name-input]")
-  //     .type("Lydia Miller-Jones")
-  //     .get("[alt='Sylvia Palmer']")
-  //     .click();
-  //   cy.contains("Save")
-  //     .click();
-  //   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  //   cy.contains(".appointment__card--show", "Sylvia Palmer");
-  // });
+  it("should book an interview", () => {
+    cy.get("[alt='Add']")
+      .first()
+      .click()
+      .get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones")
+      .get("[alt='Sylvia Palmer']")
+      .click();
+    cy.contains("Save")
+      .click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
 
   it("should edit an interview", () => {
     cy.get("[alt='Edit']")
